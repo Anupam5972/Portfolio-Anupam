798e1ef5fb58598f1fba2eb7a1e3070eb307e269
Anupam Moharana <anupam@Anupams-MacBook-Pro.local>
2026-03-22 19:55:54 +0530

    Add GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 export default defineConfig({
+  base: '/Portfolio-Anupam/',
   plugins: [
     vue(),
     vueDevTools(),
